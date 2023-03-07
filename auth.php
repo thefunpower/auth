2405fa9c90a8db76029040c9f9bb491c803296da
sunkang <13127955213@qq.com>
2023-03-07 15:16:16 +0800

    备注

--- auth.php
+++ auth.php
@@ -3,8 +3,7 @@
     Copyright (c) 2021-2031, All rights reserved.
     This is NOT a freeware
     LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
-    Connect Email: sunkangchina@163.com 
-    Code Vesion: v1.0.x
+    Connect Email: sunkangchina@163.com  
 */
 /**
  * 主要服务于后台管理员 
--- helper.php
+++ helper.php
@@ -4,8 +4,7 @@
     Copyright (c) 2021-2031, All rights reserved.
     This is NOT a freeware
     LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
-    Connect Email: sunkangchina@163.com 
-    Code Vesion: v1.0.x
+    Connect Email: sunkangchina@163.com  
 */
 
 
