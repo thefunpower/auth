3da35e76701421b757891654b0e2e68ad597c50a
sunkang <13127955213@qq.com>
2023-11-16 14:15:42 +0800

    new version

--- auth.php
+++ auth.php
@@ -1,10 +1,4 @@
 <?php 
-/*
-    Copyright (c) 2021-2031, All rights reserved.
-    This is NOT a freeware
-    LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
-    Connect Email: sunkangchina@163.com  
-*/
 /**
  * 主要服务于后台管理员 
  */ 
